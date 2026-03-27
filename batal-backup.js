20dd387dec6f97745ec7c1f4127726f52219caf6
ProMouScripts <122716237+ProMouScripts@users.noreply.github.com>
2026-03-27 08:15:09 +0200

    Update batal-backup.js

--- batal-backup.js
+++ batal-backup.js
@@ -1,14 +1,14 @@
 // window.stop_main_script = true;
 if(typeof mouscripts !== "undefined"){
    now_app_version = mouscripts.apk_version();
-   if(now_app_version == "2.9"){
-       window.stop_main_script = true;
-       download_apk_confirm = confirm("يرجي تحميل احدث اصدار من التطبيق . \n اضغط Ok للذهاب الي رابط التحميل");
+    if (["2.9", "2.9.4"].includes(now_app_version)) {
+        window.stop_main_script = true;
+        download_apk_confirm = confirm("يرجي تحميل احدث اصدار من التطبيق . \n اضغط Ok للذهاب الي رابط التحميل");
         if (download_apk_confirm) {
             mouscripts.open_external_link("https://www.elbatal-app.com");
             mouscripts.exitApp();
         } else {
             mouscripts.exitApp();
         }
-   }
+    }
 }
