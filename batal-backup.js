588d27b1cd3c90cce261817e38758910895e24d9
ProMouScripts <122716237+ProMouScripts@users.noreply.github.com>
2026-03-27 16:30:38 +0200

    Update batal-backup.js

--- batal-backup.js
+++ batal-backup.js
@@ -1,7 +1,7 @@
 // window.stop_main_script = true;
 if(typeof mouscripts !== "undefined"){
    now_app_version = mouscripts.apk_version();
-    if (["2.9", "2.9.4"].includes(now_app_version)) {
+    if (["2.9"].includes(now_app_version)) {
         window.stop_main_script = true;
         download_apk_confirm = confirm("يرجي تحميل احدث اصدار من التطبيق . \n اضغط Ok للذهاب الي رابط التحميل");
         if (download_apk_confirm) {
